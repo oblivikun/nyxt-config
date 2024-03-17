75ea29120626788d848d6466f7670f4d0357114e
Oblivikun <dudepog8@gmail.com>
2024-03-17 17:55:01 -0400

    more stuff and cruise control keys

--- config.lisp
+++ config.lisp
@@ -1,5 +1,5 @@
 
-(define-configuration (input-buffer) ((default-modes (remove-if (lambda (nyxt::m) (find (symbol-name nyxt::m) '("EMACS-MODE" "VI-NORMAL-MODE" "VI-INSERT-MODE") :test #'string=)) %slot-value%)))) (define-configuration (input-buffer) ((default-modes (pushnew 'nyxt/mode/emacs:emacs-mode %slot-value%))))
+(define-configuration (input-buffer) ((default-modes (remove-if (lambda (nyxt::m) (find (symbol-name nyxt::m) '("EMACS-MODE" "VI-NORMAL-MODE" "VI-INSERT-MODE") :test #'string=)) %slot-value%)))) 
 (defvar gruv-theme
  (make-instance 'theme:theme
  :dark-p nil
@@ -63,43 +63,67 @@
              (lambda (engine) (apply 'make-search-engine engine))
              *my-search-engines*)))))
 (defmacro alter-keyscheme (keyscheme scheme-name &body bindings)
- #+nyxt-2
- `(let ((scheme ,keyscheme))
+  #+nyxt-2
+  `(let ((scheme ,keyscheme))
      (keymap:define-key (gethash ,scheme-name scheme)
        ,@bindings)
      scheme)
- #+nyxt-3
- `(keymaps:define-keyscheme-map "custom" (list :import ,keyscheme)
+  #+nyxt-3
+  `(keymaps:define-keyscheme-map "custom" (list :import ,keyscheme)
      ,scheme-name
      (list ,@bindings)))
+
+(define-mode cruise-control-mode-emacs-map
+  nil
+  "Custom mode for cruise control bindings."
+  ((keyscheme-map
+    (alter-keyscheme %slot-value%
+                     nyxt/keyscheme:emacs
+                     "C-c v" 'velocity-zero
+                     "C-c +" 'velocity-incf
+                     "C-c =" 'velocity-decf
+                     "M-c" 'cruise-control-mode))))
+
+
 
 
 
 (define-configuration base-mode
- ((keyscheme-map
-    (alter-keyscheme %slot-value%
-      nyxt/keyscheme:emacs
-      "M-h" 'history-tree
-      "M-h b" 'buffer-history-tree))))
+    ((keyscheme-map
+      (alter-keyscheme %slot-value%
+		       nyxt/keyscheme:emacs
+		       "M-s s" 'search-buffers
+		       "M-h h" 'history-tree
+		       "M-h b" 'buffer-history-tree
+
+		       ))))
 (define-configuration :status-buffer
   "Display modes as short glyphs."
   ((glyph-mode-presentation-p t)))
 
 (define-configuration status-buffer
-((style (str:concat
-           %slot-value%
-           (theme:themed-css (theme *browser*)
-	     `("#controls,#actions"
-	       :display none !important))))))
-
+    ((style (str:concat
+             %slot-value%
+             (theme:themed-css (theme *browser*)
+			       `("#controls,#actions"
+				 :display none !important))))))
+(define-configuration input-buffer
+    ((override-map
+      (let ((map (make-keymap "override-map")))
+	(define-key map "M-x" 'execute-command)))))
 
 (defmethod format-status-load-status ((status status-buffer))
   "A fancier load status."
   (spinneret:with-html-string
-   (:span (if (and (current-buffer)
-                   (web-buffer-p (current-buffer)))
-              (case (slot-value (current-buffer) 'nyxt::status)
-                    (:unloaded "∅")
-                    (:loading "∞")
-                    (:finished ""))
-            ""))))
+      (:span (if (and (current-buffer)
+                      (web-buffer-p (current-buffer)))
+		 (case (slot-value (current-buffer) 'nyxt::status)
+                   (:unloaded "∅")
+                   (:loading "∞")
+                   (:finished ""))
+		 ""))))
+(define-configuration input-buffer
+ ((default-modes
+    (append
+      (remove-if (lambda (nyxt::m) (find (symbol-name nyxt::m) '("EMACS-MODE" "VI-NORMAL-MODE" "VI-INSERT-MODE") :test #'string=)) %slot-value%)
+      (list 'nyxt/mode/emacs:emacs-mode 'cruise-control-mode-emacs-map)))))
